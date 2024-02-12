13c1bb12fcac2387fabef61eff9c20cd95775c90
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-02-11 21:29:35 -0300

    Get code completion (nvim-cmp)

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,4 +3,5 @@ require'jdtls'.start_or_attach({
         vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls',
         ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
     },
+    capabilities = require'cmp_nvim_lsp'.default_capabilities(),
 })
--- init.lua
+++ init.lua
@@ -19,7 +19,34 @@ require("lazy").setup({
     },
     {
         'mfusseneger/nvim-jdtls',
+        dependencies = 'hrsh7th/cmp-nvim-lsp',
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        version = false,
+        dependencies = {
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        },
     },
 })
 
 require'mason'.setup()
+require'cmp'.setup({
+    snippet = {
+        expand = function(args)
+            require'luasnip'.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<C-c>'] = require'cmp'.mapping.abort(),
+        ['<CR>'] = require'cmp'.mapping.confirm(),
+        ['<C-n>'] = require'cmp'.mapping.select_next_item(),
+        ['<C-p>'] = require'cmp'.mapping.select_prev_item(),
+    },
+    sources = {
+        {
+            name = 'nvim_lsp',
+        },
+    },
+})
