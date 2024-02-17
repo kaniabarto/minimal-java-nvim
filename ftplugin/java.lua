23ec6d6784e965c16ffa0327ace9b3ae87b83be2
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-02-17 12:56:54 -0300

    Debug tests (java-debug)

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -4,5 +4,5 @@ require'jdtls'.start_or_attach({
         ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
     },
     capabilities = require'cmp_nvim_lsp'.default_capabilities(),
-    bundles = { vim.fn.expand'$HOME/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar' }
+    bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', 1), '\n'),
 })
